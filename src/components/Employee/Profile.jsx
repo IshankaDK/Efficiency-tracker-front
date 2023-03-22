b87afeb90dfab59d9c6a73d9a86095ebf0939525
IshankaDK <ishanka.d@industrialsoftwaregroup.com>
2023-03-22 23:49:31 +0530

    remove pic in profile

--- src/components/Employee/Profile.jsx
+++ src/components/Employee/Profile.jsx
@@ -2,37 +2,27 @@ import React, { useState } from "react";
 import PageHeader from "../PageHeader";
 const user = {
   name: "John Doe",
-  username: "johndoe",
+  email: "johndoe",
   password: "mypassword",
-  picture: "https://randomuser.me/api/portraits/med/men/20.jpg",
 };
 
 const Profile = () => {
   const [name, setName] = useState(user.name);
-  const [username, setUsername] = useState(user.username);
+  const [email, setEmail] = useState(user.email);
   const [password, setPassword] = useState(user.password);
-  const [picture, setPicture] = useState(user.picture);
 
-  const handleUpdate = (event) => {
-    event.preventDefault();
-    onUpdate({ name, username, password, picture });
+  const handleUpdate = () => {
+    onUpdate({ name, email, password });
   };
 
-  const onUpdate = (name, username, password, picture) => {
-    console.log(name, username, password, picture);
+  const onUpdate = (name, email, password) => {
+    console.log(name, email, password);
   };
   return (
     <>
       <PageHeader title={"Your Profile"} />
-      <div className="max-w-md mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div className="mb-4">
-          <img
-            className="rounded-full h-20 w-20 mx-auto"
-            src={picture}
-            alt="Profile"
-          />
-        </div>
-        <form onSubmit={handleUpdate}>
+      <div className="max-w-md mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 mt-20">
+        <div>
           <div className="mb-4">
             <label
               className="block text-gray-700 font-bold mb-2"
@@ -52,17 +42,17 @@ const Profile = () => {
           <div className="mb-4">
             <label
               className="block text-gray-700 font-bold mb-2"
-              htmlFor="username"
+              htmlFor="email"
             >
-              Username
+              Email
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="username"
+              id="email"
               type="text"
-              placeholder="Username"
-              value={username}
-              onChange={(event) => setUsername(event.target.value)}
+              placeholder="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
             />
           </div>
           <div className="mb-4">
@@ -81,30 +71,17 @@ const Profile = () => {
               onChange={(event) => setPassword(event.target.value)}
             />
           </div>
-          <div className="mb-4">
-          <label
-            htmlFor="picture"
-            className="block text-gray-700 font-bold mb-2"
-          >
-            Profile Picture
-          </label>
-          <input
-            type="file"
-            id="picture"
-            name="picture"
-            onChange={(e) => setPicture(e.target.files[0])}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
+          <div className="mb-4"></div>
           <div className="flex items-center justify-between">
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="submit"
+              type="button"
+              onClick={() => handleUpdate()}
             >
               Save
             </button>
           </div>
-        </form>
+        </div>
       </div>
     </>
   );
