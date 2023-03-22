604af84f2fc543124f3f6b1a76e4604acf3667ba
IshankaDK <ishanka.d@industrialsoftwaregroup.com>
2023-03-22 23:49:19 +0530

    placeholder name

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -53,7 +53,7 @@ const Login = () => {
               name="username"
               id="username"
               className="w-full py-4 px-8 bg-slate-200 placeholder:font-semibold rounded hover:ring-1 outline-blue-500"
-              placeholder="User Name"
+              placeholder="Name or User Name"
               onChange={handleChange}
             />
           </div>
