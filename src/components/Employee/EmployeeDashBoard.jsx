ec1f0331ab8f66beb692886045ea9626327a5a09
IshankaDK <ishanka.d@industrialsoftwaregroup.com>
2023-03-24 02:52:05 +0530

    employee dashboard vh

--- src/components/Employee/EmployeeDashBoard.jsx
+++ src/components/Employee/EmployeeDashBoard.jsx
@@ -6,7 +6,7 @@ const EmployeeDashBoard = ({ children }) => {
   return (
     <>
       <EmployeeHeader />
-      <div>{children}</div>
+      <div className="h-screen">{children}</div>
       <div className="fixed bottom-0 w-full">
         <Footer />
       </div>
